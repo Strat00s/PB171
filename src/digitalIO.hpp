b46604d4457e4fdd5ecee8f5851f88e857bf70b1
stratos <l.bastyr@seznam.cz>
2023-06-11 19:49:08 +0200

    digitalIO

--- src/digitalIO.hpp
+++ src/digitalIO.hpp
@@ -2,25 +2,27 @@
 
 #define LOW 0
 #define HIGH 1
+
 #define INPUT 0
 #define OUTPUT 1
 
 
-void pinMode(uint8_t port, uint8_t pin, uint8_t mode) {
+void pinMode(volatile uint8_t& ddr, uint8_t pin, uint8_t mode) {
     if (mode == INPUT) {
         // set bit to 0 for input
-        port &= ~(1 << pin);
-    } else {
+        ddr &= ~(1 << pin);
+    }
+    else {
         // set bit to 1 for output
-        port |= (1 << pin);
+        ddr |= (1 << pin);
     }
 }
 
-uint8_t digitalRead(uint8_t port, uint8_t pin) {
+uint8_t digitalRead(volatile uint8_t& port, uint8_t pin) {
     return (port & (1 << pin)) ? HIGH : LOW;
 }
 
-void digitalWrite(uint8_t port, uint8_t pin, uint8_t value) {
+void digitalWrite(volatile uint8_t& port, uint8_t pin, uint8_t value) {
     if (value == LOW) {
         // clear bit
         port &= ~(1 << pin);
